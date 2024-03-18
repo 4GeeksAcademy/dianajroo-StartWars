175d3cb19f112f6b6426f279363c216ef50de259
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-03-18 18:00:58 +0000

    startwarsPaso1

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import startWarsimg from "../../img/start-wars.jpg";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<div classNameName="container">
+			<nav classNameName="navbar navbar-light bg-light mb-3">
+				<Link to="/">
+					<span classNameName="navbar-brand mb-0 h1"> <img src={startWarsimg} style={{ width: '100px', height: '100px,' }} />  </span>
 				</Link>
-			</div>
-		</nav>
+				<div classNameName="ml-auto">
+					<div className="dropdown">
+						<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Dropdown link
+						</a>
+
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#">Action</a></li>
+							<li><a className="dropdown-item" href="#">Another action</a></li>
+							<li><a className="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
 	);
+
 };
