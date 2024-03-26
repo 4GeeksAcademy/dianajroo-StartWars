ced1bf1aa4ad85974d82929a56f7fd1aa86a272b
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-03-26 23:52:00 +0000

    layoutlisto

--- src/js/views/LearnMore.js
+++ src/js/views/LearnMore.js
@@ -24,28 +24,42 @@ const LearnMore = () => {
     return (
 
 
-        <div className="container" style={{ display: 'flex', justifyContent: 'center' }}>
-            <div className="body">
+        <div className="container">
+
+
+            <div className="body" style={{ display: 'flex', justifyContent: 'space-between' }}>
+
 
 
                 <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
-                {learnMore && 
-                <div>
-                    <h1>{learnMore.properties.name}</h1>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit volutatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae hjdue ndhu nsbsh ki nsbgtwsk nklspw ngdyej pspjses nhuk njshy.Nysmdujd kzops nb jshhi bsgwu ñapw ndhd bhd, njdkieue hkwamsndhe dkdkc.bhuiwuoijjdhui bigiuqwdohudjd mhduihie nchud kjduihuedebe hbduhdhbuygc jhbdhudb,jkhbdhuhuijew.Nhbhchchiuwhfi hbdij mncjd jkurbfhh.</p>
-                 </div>
-                }
-            </div>
-            <div className="footer">
                 {learnMore &&
                     <div>
-                        <p>{learnMore.properties.name}</p>
-                        <p>{learnMore.properties.birth_year}</p>
-                        <p>{learnMore.properties.gender}</p>
-                        <p>{learnMore.properties.height}</p>
-                        <p>{learnMore.properties.skin_color}</p>
-                        <p>{learnMore.properties.eye_color}</p>
+                        <h1>{learnMore.properties.name}</h1>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit volutatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae hjdue ndhu nsbsh ki nsbgtwsk nklspw ngdyej pspjses nhuk njshy.Nysmdujd kzops nb jshhi bsgwu ñapw ndhd bhd, njdkieue hkwamsndhe dkdkc.bhuiwuoijjdhui bigiuqwdohudjd mhduihie nchud kjduihuedebe hbduhdhbuygc jhbdhudb,jkhbdhuhuijew.Nhbhchchiuwhfi hbdij mncjd jkurbfhh.</p>
+                    </div>
+                }
+            </div>
+            <div className="footer" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+                {learnMore &&
+                    <div className="row"  >
+                        <h2>
+                            Name
+                            Birt Year
+                            Gender
+                            Height
+                            Skin Color
+                            Eye Color
+                           
 
+                        </h2>
+                        <h5 style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            {learnMore.properties.name}
+                            {learnMore.properties.birth_year}
+                            {learnMore.properties.gender}
+                            {learnMore.properties.height}
+                            {learnMore.properties.skin_color}
+                            {learnMore.properties.eye_color}
+                        </h5>
                     </div>
                 }
             </div>
